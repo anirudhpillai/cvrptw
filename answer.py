b075d6d6b30125ab2124ed977494125674edcded
Anirudh <anirudhx5@gmail.com>
2018-01-27 18:49:21 +0000

    clustered up

--- answer.py
+++ answer.py
@@ -1,6 +1,9 @@
 import csv
 import math
 import pandas as pd
+from sklearn.cluster import KMeans
+import numpy as np
+
 
 
 MAX_DELIVERIES = 10
@@ -9,22 +12,11 @@ PRICE_PER_TRUCK = 100
 PRICE_PER_KM = 0.061
 MAX_TIME_PER_TRUCK = 10
 
+TOTAL_CLUSTERS = 70
+
 
 def distance(x1, y1, x2, y2):
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
-
-# class Graph:
-#     def __init__(self, orders):
-#         self.matrix = {}
-#
-#         for o1 in orders:
-#             self.matrix[o1.id] = {}
-#
-#             for o2 in orders:
-#                 self.matrix[o1.id][o2.id] = distance(
-#                     o1.x, o1.y, o2.x, o2.y
-#                 )
 
 
 class Truck:
@@ -32,7 +24,6 @@ class Truck:
 
     def __init__(self):
         self.capacity_left = MAX_DELIVERIES
-        # self.distance_travelled = 0
         self.x = 0
         self.y = 0
         self.current_time = 0
@@ -114,19 +105,50 @@ def bruteforce(orders):
     return deliveries
 
 
+def clustered(orders, kmeans):
+    clusters = {i: np.where(kmeans.labels_ == i)[0] for i in range(kmeans.n_clusters)}
+    deliveries = []
+
+    # loop over each cluster
+    for cluster in clusters.values():
+        curr_len = len(deliveries)
+        current_truck = Truck()
+
+        for idx in cluster:
+            order = orders[idx]
+            if current_truck.can_deliver_order(order):
+                current_truck.deliver_order(order)
+            else:
+                deliveries.extend(current_truck.deliveries[::])
+                current_truck = Truck()
+                current_truck.deliver_order(order)
+
+        if len(deliveries) - curr_len < len(cluster):
+            deliveries.extend(current_truck.deliveries[::])
+
+    return deliveries
+
+
 def answer():
-    # Columns are: order_id, x, y time_window_start
     df = pd.read_csv("orders.csv")
 
+    orders_to_cluster = []
     orders = []
+
     for _, row in df.iterrows():
-        orders.append(Order(
+        orders_to_cluster.append([row["x"], row["y"]])
+        order = Order(
             row["order_id"], row["x"], row["y"], row["time_window_start"]
-        ))
+        )
+        orders.append(order)
+
+    kmeans = KMeans(n_clusters=TOTAL_CLUSTERS, random_state=0)
+    kmeans.fit(orders_to_cluster)
 
     print(len(orders))
 
-    ans_list = bruteforce(orders)
+    # ans_list = bruteforce(orders)
+    ans_list = clustered(orders, kmeans)
 
     print(len(ans_list))
 
@@ -134,14 +156,7 @@ def answer():
         writer = csv.writer(csvfile)
         writer.writerow(["truck_id", "order_id", "sequence_number"])
         for row in ans_list:
-            # writer.writerow("%d, %d, %d" % (t_id, o_id, s_no))
             writer.writerow(row)
-
-    # graph = Graph(orders)
-    # print(graph.matrix)
-
-    # write answer to file
-    # orders.to_csv("answer.txt")
 
 
 answer()
